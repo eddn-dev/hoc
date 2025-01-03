2e509657e145a18599ccfb0a95593d87f052abaa
Hatziry Vitales Herrera <hatziryvh@gmail.com>
2025-01-02 19:38:20 -0600

    Hoc 5

---
+++ src/hoc/HOC.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hoc;
+
+import javax.swing.JFrame;
+import hoc.IDE; // Asegúrate de importar tu clase IDE correctamente
+
+/**
+ *
+ * @author chori
+ */
+public class HOC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Crear una instancia del formulario IDE
+        IDE ide = new IDE();
+        
+         // Configurar el formulario desde la clase principal
+        ide.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximizar la ventana
+        ide.setVisible(true); // Mostrar la ventana
+        ide.setLocationRelativeTo(null); // Centrarla en pantalla (opcional)
+    }
+}
